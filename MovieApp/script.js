a8511de8a01bf26f0a7835d152d60976b916bbde
Vankoekelberg <Mathis>
2021-08-12 10:18:34 +0200

    edit

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -4,6 +4,14 @@ const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 let pageNumber = 1;
 let cardsTag = document.getElementById("cardsTag");
+let stockData;
+let count;
+let card;
+let cardTitle;
+let cardScore;
+let cardImg;
+let divTitle;
+let cardDes;
 // let cardsTagSearch = document.getElementById("cardsTagSearch");
 // let searchTag = document.getElementById("searchTag");
 
@@ -18,27 +26,20 @@ function getMovie() {
     fetch(APIURL + pageNumber)
     .then((res) => res.json())
     .then((data) => {
-        for(let count = 0; count < 20; count++) {
-            let card = document.createElement("div");
-            let cardTitle = document.createElement("p");
-            let cardScore = document.createElement("p");
-            let cardImg = document.createElement("img");
-            let divTitle = document.createElement("div");
-            let cardDes = document.createElement("p");
+        stockData = data;
+        console.log(stockData);
+        for(count = 0; count < 20; count++) {
+            card = document.createElement("div");
+            cardTitle = document.createElement("p");
+            cardScore = document.createElement("p");
+            cardImg = document.createElement("img");
+            divTitle = document.createElement("div");
+            cardDes = document.createElement("p");
             divTitle.classList.add("nameFlex");
             
             card.classList.add("cardMovie");
             cardTitle.innerHTML = data.results[count].title;
             cardTitle.classList.add("movieTitle");
-
-            cardScore.innerHTML = data.results[count].vote_average;
-            if(data.results[count].vote_average < 3) {
-                cardScore.classList.add("movieScore1");
-            } else if (data.results[count].vote_average <= 6 && data.results[count].vote_average > 3) {
-                cardScore.classList.add("movieScore2");
-            } else if (data.results[count].vote_average <= 10 && data.results[count].vote_average > 6) {
-                cardScore.classList.add("movieScore3");
-            }
 
             if(data.results[count].poster_path === null) {
                 cardImg.src = "";
@@ -57,12 +58,43 @@ function getMovie() {
             divTitle.appendChild(cardTitle);
             divTitle.appendChild(cardScore);
             cardsTag.appendChild(card);
+            getScore();
+            getID();
         }
         pageCheck();
-        // console.log(data.results[1].vote_average);
-        // console.log(data);
+        console.log(data);
     });
 }
+
+function getScore() {
+    let cardScore = document.createElement("p");
+    cardScore.innerHTML = stockData.results[count].vote_average;
+    if(stockData.results[count].vote_average < 3) {
+        cardScore.classList.add("movieScore1");
+    } else if (stockData.results[count].vote_average <= 6 && stockData.results[count].vote_average > 3) {
+        cardScore.classList.add("movieScore2");
+    } else if (stockData.results[count].vote_average <= 10 && stockData.results[count].vote_average > 6) {
+        cardScore.classList.add("movieScore3");
+    }
+    divTitle.appendChild(cardScore);
+}
+
+function getID() {
+    let idFlex = document.createElement("div");
+    let cardId = document.createElement("p");
+    let cardLang = document.createElement("p");
+    idFlex.classList.add("idFlex");
+    cardId.classList.add("cardId");
+    cardLang.classList.add("cardLang");
+
+    cardId.innerHTML = "ID: " + stockData.results[count].id;
+    cardLang.innerHTML = stockData.results[count].original_language;
+
+    idFlex.appendChild(cardId);
+    idFlex.appendChild(cardLang);
+    card.appendChild(idFlex);
+}
+
 
 function pageCheck() {
     if(pageNumber != 20) {
